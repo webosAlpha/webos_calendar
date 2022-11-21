92cea645836f382d7a113308453a5b9ab4e1d008
hanjaegyeong <hjg000223@naver.com>
2022-11-22 03:23:46 +0900

    feat: data.json 생성

--- server/routes/spreadSheet/sheetRouter.js
+++ server/routes/spreadSheet/sheetRouter.js
@@ -2,13 +2,11 @@ const express = require("express");
 const router = express.Router();
 const Sheet = require("../../schemas/sheet");
 const ctrl = require("./sheet.ctrl");
-
-ctrl.getSheetData("webostest");
+const data = require("./data.json");
 
 router.get("/plz", async (req, res) => {
 	try {
-		const accounts = await Sheet.find();
-		res.json(accounts);
+		res.json(data);
 	} catch (err) {
 		res.status(500).json({ message: err.message });
 	}
