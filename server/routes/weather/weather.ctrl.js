783aede857a3068b990bc3a1bc87293961395cc4
cookie0010 <sg.been2017@gmail.com>
2022-12-11 14:59:52 +0900

    weather 일정 시간에 저장

--- server/routes/weather/weather.ctrl.js
+++ server/routes/weather/weather.ctrl.js
@@ -1,33 +1,17 @@
-const parse = require("./weather.parse");
 const Weather = require("../../schemas/weather");
-const weatherData = require("../../weatherData.json");
-
-const plzWeather = async (req, res) => {
-	try {
-		parse.load();
-		res.json(weatherData);
-	} catch (err) {
-		res.status(500).json({ message: err.message });
-	}
-};
 
 const getWeather = async (req, res) => {
 	try {
-		const weatherList = [];
-		const year = req.query.year;
-		const month = req.query.month;
-		const day = req.query.day;
-
-		parse.load();
-		weatherData.forEach((e) => {
-			e.year == year && e.month == month && e.day == day
-				? weatherList.push(e)
-				: null;
+		const weather = await Weather.find({
+			year: req.query.year,
+			month: req.query.month,
+			day: req.query.day,
 		});
-		res.json(weatherList);
+
+		res.json(weather);
 	} catch (err) {
 		res.status(500).json({ message: err.message });
 	}
 };
 
-module.exports = { getWeather, plzWeather };
+module.exports = { getWeather };
--- server/routes/weather/weatherRouter.js
+++ server/routes/weather/weatherRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const ctrl = require("./weather.ctrl");
 
-router.get("/plz", ctrl.plzWeather);
 router.get("/", ctrl.getWeather);
 
 module.exports = router;
